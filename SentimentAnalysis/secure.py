d4998a91847d57845ecb1b2c66554abd8272db3f
flirtguru <samarthjsh300@gmail.com>
2017-12-29 23:30:32 +0530

    first commit

---
+++ SentimentAnalysis/secure.py
@@ -0,0 +1,5 @@
+#get your own api tokens by creating an app in the twitter API
+consumer_key='xxxxxxxxxxxxxxxxxxxxxxx'
+consumer_secret='xxxxxxxxxxxxxxxxxxxx'
+api='xxxxxxxxxxxxxxxxxxxxx'
+api_secret='xxxxxxxxxxxxxxxxxx'
